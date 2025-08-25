3155b8df04f032e3f219ad9422922822ccf7c782
Phil Mike Jones <philmikejones@gmail.com>
2025-08-25 21:39:15 +0100

    get api date

--- get_data_police.py
+++ get_data_police.py
@@ -1,15 +1,16 @@
 import requests
+import json
 
-# Documentation from:
-# https://data.police.uk/docs/method/crime-street/
+## Documentation from:
+## https://data.police.uk/docs/method/crime-street/
+## Build a query with:
+## base_url + crime_type + date + coords
+## It will return all crimes for the given month within a 1 mile radius of the coords
 
-# Example request with poly area:
-# https://data.police.uk/api/crimes-street/all-crime?date=2024-01&lat=52.629729&lng=-1.131592
-
-# Replace with your API endpoint
 base_url = "https://data.police.uk/api/crimes-street/"
-crime_type = "burglary"
-# crime_type choices:
+
+## crime_type choices:
+## uses `all-crime` by default unless specified
 # all-crime
 # anti-social-behaviour
 # bicycle-theft
@@ -26,11 +27,33 @@ crime_type = "burglary"
 # violent-crime
 # other-crime
 
-date = "2025-01"
-lat = 53.1282
-lng = -1.2677
+## Date
+## Specify a date in the format YYYY-MM
+## By default uses most recent month data is available for
 
-query_url = base_url + crime_type + "?date=" + date + "&lat=" + str(lat) + "&lng=" + str(lng)
+def get_api_date() -> str:
+    date_url = "https://data.police.uk/api/crime-last-updated"
+    response = requests.get(date_url)
+    if response.status_code != 200:
+        api_error = f"Error in get_api_date(): {response.status_code} - {response.text}"
+        return api_error
+    response = response.json()
+    return response
+
+def truncate_day(date_str):
+    # Assumes input format is 'YYYY-MM-DD'
+    return date_str[:7]
+
+
+# Example request with poly area:
+# https://data.police.uk/api/crimes-street/all-crime?date=2024-01&lat=52.629729&lng=-1.131592
+
+
+
+#lat = 53.1282
+#lng = -1.2677
+#
+#query_url = base_url + crime_type + "?date=" + date + "&lat=" + str(lat) + "&lng=" + str(lng)
 
 def get_resource():
     response = requests.get(query_url)
@@ -41,5 +64,5 @@ def get_resource():
         return None
 
 if __name__ == "__main__":
-    result = get_resource()
-    print(query_url)
+    date = get_api_date()
+    print(date)
