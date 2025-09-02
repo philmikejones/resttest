b093bd644629335f60141e0d1945d202d0a95635
Phil Mike Jones <philmikejones@gmail.com>
2025-09-02 13:55:46 +0100

    returns date of API

--- get_data_police.py
+++ get_data_police.py
@@ -1,48 +1,30 @@
 import requests
-import json
 
 ## Documentation from:
-## https://data.police.uk/docs/method/crime-street/
-## Build a query with:
-## base_url + crime_type + date + coords
-## It will return all crimes for the given month within a 1 mile radius of the coords
+# https://data.police.uk/docs/method/crime-street/
+# Build a query with:
+# base_url + crime_type + date + coords
+# It will return all crimes for the given month within a 1 mile radius of the coords
 
 base_url = "https://data.police.uk/api/crimes-street/"
 
-## crime_type choices:
-## uses `all-crime` by default unless specified
-# all-crime
-# anti-social-behaviour
-# bicycle-theft
-# burglary
-# criminal-damage-arson
-# drugs
-# other-theft
-# possession-of-weapons
-# public-order
-# robbery
-# shoplifting
-# theft-from-the-person
-# vehicle-crime
-# violent-crime
-# other-crime
-
 ## Date
-## Specify a date in the format YYYY-MM
-## By default uses most recent month data is available for
-
 def get_api_date() -> str:
+    """
+    Obtains the date of the latest API data
+    
+    Returns:
+    str A date in the format YYYY-MM
+    """
     date_url = "https://data.police.uk/api/crime-last-updated"
     response = requests.get(date_url)
     if response.status_code != 200:
         api_error = f"Error in get_api_date(): {response.status_code} - {response.text}"
         return api_error
-    response = response.json()
+    response = response.json().get('date')
+    response = response[:7]  # YYYY-MM-DD is returned but it expects YYYY-MM
     return response
 
-def truncate_day(date_str):
-    # Assumes input format is 'YYYY-MM-DD'
-    return date_str[:7]
 
 
 # Example request with poly area:
